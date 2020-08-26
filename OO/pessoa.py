6056d83c593b121104ed478d9fc47bff2d1c868d
Paulo-dev2 <abdiel.paulo@outlook.com>
2020-08-26 17:04:14 -0300

    Criado o metódo cumprimentar.

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,4 +1,7 @@
 class Pessoa:
+    def __init__(self):
+        self.nome = None
+
     def cumprimentar(self):
         return f'Olá Mundo {id(self)}'
 
@@ -6,4 +9,5 @@ if __name__ == '__main__':
     p = Pessoa()
     print(Pessoa.cumprimentar(p))
     print(id(p))
-    print(p.cumprimentar())
+    p.nome = 'Paulo'
+    print(p.nome)
